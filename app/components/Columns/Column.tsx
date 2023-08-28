f69987b1c0be400ac4819d6d370992fbabdac52e
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-08-28 21:01:32 +0300

    Add column effects on hover and a tooltip

--- app/components/Columns/Column.tsx
+++ app/components/Columns/Column.tsx
@@ -7,7 +7,14 @@ interface ColumnProps {
 const Column = ({ value }: ColumnProps) => {
     const columnStyle = value === 560 ? styles.item__small : styles.item__large;
 
-    return <div className={styles.item + " " + columnStyle}></div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.item + " " + columnStyle}></div>
+            <div className={styles.tooltip}>
+                <span>{value}</span>
+            </div>
+        </div>
+    );
 };
 
 export default Column;
